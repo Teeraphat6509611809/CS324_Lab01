0a173a0696545f1e503fb95477325dc5ebe392ad
teeraphat <teeraphat.sir@dome.tu.ac.th>
2025-03-16 18:18:30 +0700

    Add script to run by ./run_x

--- CS324_Security_Lab01-Cryptography-RSA_G01_task-2-5.c
+++ CS324_Security_Lab01-Cryptography-RSA_G01_task-2-5.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <openssl/bn.h>
+
+#define NBIT 128
+
+void printBN(char *msg, BIGNUM * a)
+{
+/* Use BN_bn2hex(a) for hex string
+* Use BN_bn2dec(a) for decimal string */
+char * number_str = BN_bn2hex(a);
+printf("%s %s\n", msg, number_str);
+OPENSSL_free(number_str);
+}
+
+int main(){
+    
+}
