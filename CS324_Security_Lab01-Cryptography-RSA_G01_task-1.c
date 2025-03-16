aabc1a1f2a5dbbcf985ece1a2b0b054ca1b487f4
teeraphat <teeraphat.sir@dome.tu.ac.th>
2025-03-16 18:10:53 +0700

    Init file

---
+++ CS324_Security_Lab01-Cryptography-RSA_G01_task-1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <openssl/bn.h>
+
+#define NBIT 128
+
+void printBN(char *msg, BIGNUM * a)
+{
+/* Use BN_bn2hex(a) for hex string
+* Use BN_bn2dec(a) for decimal string */
+char * number_str = BN_bn2dec(a);
+printf("%s %s\n", msg, number_str);
+OPENSSL_free(number_str);
+}
+
+
+int main(){
+    // const char *P_STR = "F7E75FDC469067FFDC4E847C51F452DF";
+    // const char *Q_STR = "E85CED54AF57E53E092113E62F436F4F";
+    // const char *E_STR = "0D88C3";
+    const char *P_STR = "7";
+    const char *Q_STR = "B";
+    const char *E_STR = "D";
+    BN_CTX *ctx = BN_CTX_new();
+    BIGNUM *p = BN_new();
+    BIGNUM *q = BN_new();
+    BIGNUM *e = BN_new();
+    BIGNUM *n = BN_new();
+    BIGNUM *phi_n = BN_new();
+    BIGNUM *one = BN_new();
+    BIGNUM *result = BN_new();
+    BIGNUM *pkey = BN_new();
+    BN_hex2bn(&one , "1");
+
+    BN_hex2bn(&p , P_STR);
+    BN_hex2bn(&q , Q_STR);
+    BN_mul(n , p , q , ctx);
+
+    BN_sub(p ,p , one);
+    BN_sub(q ,q , one);
+
+    BN_hex2bn(&e , E_STR);
+    BN_mul(phi_n , p , q , ctx);
+    
+    BN_mod_inverse(pkey , e , phi_n , ctx);
+    printBN("PKeys :" , pkey);
+
+
+
+}
